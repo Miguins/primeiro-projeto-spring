ea5aec10279e9c2e65bb1b61f40bedb83cbde2c1
Lucas Miguins <lucasmiguins@gmail.com>
2019-12-19 17:55:25 -0300

    Primeiro commit

---
+++ src/main/java/com/example/api/PrimeiroProjetoApplication.java
@@ -0,0 +1,14 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
+	}
+
+}
